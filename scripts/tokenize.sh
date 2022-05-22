65f349f81ede5c6d4b0f9699c39d2369c68a41c3
edArcturus <eyalliron.dolev@uzh.ch>
2022-05-22 11:47:44 +0200

    Forgot sth

--- scripts/tokenize.sh
+++ scripts/tokenize.sh
@@ -2,9 +2,9 @@ scripts=`dirname "$0"`
 base=$scripts/..
 data=$base/data
 
-python3 tokenize.py $data/dev.de-nl.nl > $data/dev_tok.de-nl.nl
-python3 tokenize.py $data/dev.de-nl.de > $data/dev_tok.de-nl.de
-python3 tokenize.py $data/test.de-nl.nl > $data/test_tok.de-nl.nl
-python3 tokenize.py $data/test.de-nl.de > $data/test_tok.de-nl.de
-python3 tokenize.py $data/train.de-nl.nl > $data/train_tok.de-nl.nl
-python3 tokenize.py $data/train.de-nl.de > $data/train_tok.de-nl.de
+python3 $scripts/tokenize.py $data/dev.de-nl.nl > $data/dev_tok.de-nl.nl
+python3 $scripts/tokenize.py $data/dev.de-nl.de > $data/dev_tok.de-nl.de
+python3 $scripts/tokenize.py $data/test.de-nl.nl > $data/test_tok.de-nl.nl
+python3 $scripts/tokenize.py $data/test.de-nl.de > $data/test_tok.de-nl.de
+python3 $scripts/tokenize.py $data/train.de-nl.nl > $data/train_tok.de-nl.nl
+python3 $scripts/tokenize.py $data/train.de-nl.de > $data/train_tok.de-nl.de
