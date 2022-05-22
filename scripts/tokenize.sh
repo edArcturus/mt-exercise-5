0f87d736d844289daa8fd91c99793091570b694b
Eyal Dolev <eyal.l.dolev@gmail.com>
2022-05-22 13:56:17 +0200

    Fix filenames

--- scripts/tokenize.sh
+++ scripts/tokenize.sh
@@ -2,9 +2,9 @@ scripts=`dirname "$0"`
 base=$scripts/..
 data=$base/data
 
-python3 $scripts/tokenize.py $data/dev.de-nl.nl > $data/dev_tok.de-nl.nl
-python3 $scripts/tokenize.py $data/dev.de-nl.de > $data/dev_tok.de-nl.de
-python3 $scripts/tokenize.py $data/test.de-nl.nl > $data/test_tok.de-nl.nl
-python3 $scripts/tokenize.py $data/test.de-nl.de > $data/test_tok.de-nl.de
-python3 $scripts/tokenize.py $data/train.de-nl.nl > $data/train_tok.de-nl.nl
-python3 $scripts/tokenize.py $data/train.de-nl.de > $data/train_tok.de-nl.de
+python3 $scripts/tokenize.py $data/dev.de-nl.nl --lang nl > $data/dev_tok.nl
+python3 $scripts/tokenize.py $data/dev.de-nl.de --lang de > $data/dev_tok.de
+python3 $scripts/tokenize.py $data/test.de-nl.nl --lang nl > $data/test_tok.nl
+python3 $scripts/tokenize.py $data/test.de-nl.de --lang de > $data/test_tok.de
+python3 $scripts/tokenize.py $data/train.de-nl.nl --lang nl > $data/train_tok.nl
+python3 $scripts/tokenize.py $data/train.de-nl.de --lang de > $data/train_tok.de
