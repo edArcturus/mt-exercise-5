cf12ce29b75576348cb954362d24a54e91aa1e39
Eyal Dolev <eyal.l.dolev@gmail.com>
2022-05-22 11:24:31 +0200

    First commit

---
+++ scripts/tokenize.py
@@ -0,0 +1,21 @@
+import argparse
+from mosestokenizer import *
+import sys
+import logging
+
+
+if __name__ == "__main__":
+
+
+	parser = argparse.ArgumentParser(description="Simple script for tokenizing")
+	parser.add_argument("file", type=argparse.FileType('r', encoding='utf-8'))
+	parser.add_argument("--lang", type=str, choices=['de', 'en'], default='en')
+	args = parser.parse_args()
+
+	tokenize = MosesTokenizer(args.lang)
+
+	for line in args.file:
+		tokens = " ".join(tokenize(line))
+		sys.stdout.write(tokens + '\n')
+
+
