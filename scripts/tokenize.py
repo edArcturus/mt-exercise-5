0aead28f452ddac9f15f518c7a8212fe5a720252
Eyal Dolev <eyal.l.dolev@gmail.com>
2022-05-22 13:55:43 +0200

    Add more languages to choices

--- scripts/tokenize.py
+++ scripts/tokenize.py
@@ -9,7 +9,7 @@ if __name__ == "__main__":
 
 	parser = argparse.ArgumentParser(description="Simple script for tokenizing")
 	parser.add_argument("file", type=argparse.FileType('r', encoding='utf-8'))
-	parser.add_argument("--lang", type=str, choices=['de', 'en'], default='en')
+	parser.add_argument("--lang", type=str, choices=['de', 'en', 'it', 'fr', 'nl'], default='en')
 	args = parser.parse_args()
 
 	tokenize = MosesTokenizer(args.lang)
