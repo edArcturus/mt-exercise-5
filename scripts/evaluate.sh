144b1cc7eed70e9ff4ed8dc37f64f459bd79d700
Eyal Dolev <eyal.l.dolev@gmail.com>
2022-05-24 21:12:31 +0200

    Adjust paths

--- scripts/evaluate.sh
+++ scripts/evaluate.sh
@@ -11,19 +11,19 @@ translations=$base/translations
 mkdir -p $translations
 
 src=de
-trg=en
+trg=nl
 
 # cloned from https://github.com/bricksdont/moses-scripts
 MOSES=$base/tools/moses-scripts/scripts
 
 num_threads=4
-device=0
+device=1
 
 # measure time
 
 SECONDS=0
 
-model_name=transformer_sample_config
+model_name=transformer_bpe
 
 echo "###############################################################################"
 echo "model_name $model_name"
@@ -32,7 +32,7 @@ translations_sub=$translations/$model_name
 
 mkdir -p $translations_sub
 
-CUDA_VISIBLE_DEVICES=$device OMP_NUM_THREADS=$num_threads python -m joeynmt translate $configs/$model_name.yaml < $data/test.bpe.$src > $translations_sub/test.bpe.$model_name.$trg
+CUDA_VISIBLE_DEVICES=$device OMP_NUM_THREADS=$num_threads python -m joeynmt translate $configs/$model_name.yaml < $data/test_bpe.$src > $translations_sub/test.bpe.$model_name.$trg
 
 # undo BPE
 
@@ -44,7 +44,7 @@ cat $translations_sub/test.tokenized.$model_name.$trg | $MOSES/tokenizer/detoken
 
 # compute case-sensitive BLEU on detokenized data
 
-cat $translations_sub/test.$model_name.$trg | sacrebleu $data/test.$trg
+cat $translations_sub/test.$model_name.$trg | sacrebleu $data/test_bpe.$trg
 
 
 echo "time taken:"
